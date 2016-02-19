4030757da8e1304c349d9e6b1adee7d3165f1903
vitaliy-novik <vitaliy.novik.94@gmail.com>
2016-02-19 21:30:26 +0300

    Create ORM project

---
+++ ORM/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using ORM.Entities;
+using System.Data.Entity;
+
+namespace ORM
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+            : base("DataContext")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<User> ToDoLists { get; set; }
+        public DbSet<User> ToDoItems { get; set; }
+
+    }
+}

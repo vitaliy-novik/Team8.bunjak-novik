00291f1d19d55180bd729173c632b0a1417e5f2b
Maxim <2>
2016-03-01 14:03:56 +0300

    edit JS in sing up

--- WebAPI/Controllers/AccountController.cs
+++ WebAPI/Controllers/AccountController.cs
@@ -328,7 +328,7 @@ namespace WebAPI.Controllers
                 return BadRequest(ModelState);
             }
 
-            var user = new ApplicationUser() { UserName = model.Email, Email = model.Email };
+            var user = new ApplicationUser() { UserName = model.Name, Email = model.Email };
 
             IdentityResult result = await UserManager.CreateAsync(user, model.Password);
 
--- WebAPI/Models/AccountBindingModels.cs
+++ WebAPI/Models/AccountBindingModels.cs
@@ -35,19 +35,15 @@ namespace WebAPI.Models
     public class RegisterBindingModel
     {
         [Required]
-        [Display(Name = "Email")]
+        public string Name { get; set; }
+
+        [Required]
         public string Email { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
         public string Password { get; set; }
-
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-        public string ConfirmPassword { get; set; }
     }
 
     public class RegisterExternalBindingModel

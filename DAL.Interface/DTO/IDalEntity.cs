d91dfa81b740f6694bdb4d16d186514de0806faf
Maxim <2>
2016-03-06 13:10:23 +0300

    delete old prodgects

--- DAL.Interface/DTO/IDalEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace DAL.Interface.DTO
-{
-    public interface IDalEntity
-    {
-        Guid Id { get; set; }
-    }
-}
--- DAL.Interface/Repositories/IUserRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using DAL.Interface.DTO;
-
-namespace DAL.Interface.Repositories
-{
-    public interface IUserRepository : IRepository<UserDTO>
-    {
-    }
-}

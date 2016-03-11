2216c5928f54fffff6428050c6dc47534fd2f825
Maxim <2>
2016-03-11 21:05:51 +0300

    Add edit profile

---
+++ Entities/TaskList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class TaskList : Entity
+    {
+        public TaskList()
+        {
+            Items = new HashSet<TaskItem>();
+        }
+
+        public string Name { get; set; }
+        public virtual ICollection<TaskItem> Items { get; set; }
+        public User User { get; set; }
+    }
+}

b63f94053a873c8238a44b11bb14aef07b432348
Maxim <2>
2016-03-14 22:08:27 +0300

    Create profile

---
+++ Wunderlist.Repositories/TaskListRepository.cs
@@ -0,0 +1,12 @@
+﻿using Entities;
+using System.Data.Entity;
+using Wunderlist.InterfaceRepositories;
+
+namespace Wunderlist.Repositories
+{
+    public class TaskListRepository : Repository<TaskList>, IToDoListRepository
+    {
+        public TaskListRepository(DbContext context) : base(context) { }
+
+    }
+}
